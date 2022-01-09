e69b0f4b7bb4b7a6f0130fe4c4e245809ae24d3e
jarney99 <95194554+jarney99@users.noreply.github.com>
2022-01-08 18:21:21 -0600

    Modified version of PyPoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -16,6 +16,17 @@ with open(file_to_load) as election_data:
     # Read and print the header row.
     headers = next(file_reader)
     print(headers)
+   # Print the file object.
+    print(election_data)
 
+# Using the open() function with the "w" mode we will write data to the file.
+open(file_to_save, "w")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+# Use the open statement to open the file as a text file.
+with open(file_to_save, "w") as txt_file:
+    
+    # Write three counties to the file
+    txt_file.write("Counties in the election\n-------------------\nArapahoe\nDenver\nJefferson")
 
     
